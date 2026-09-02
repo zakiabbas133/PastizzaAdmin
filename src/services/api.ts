a6ef4cba1da9624a81971e6654393c008fc8dd88
zakiabbas133 <zaki.abbas@intercargo.com>
2026-09-02 16:02:16 +0100

    Consoles removed

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,9 +12,6 @@ import type { LoginResponse } from "../types/auth";
 
 export const baseUrl = import.meta.env.VITE_BASE_URL;
 
-console.log(baseUrl);
-
-
 const rawBaseQuery = fetchBaseQuery({
   baseUrl,
 
